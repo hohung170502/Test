31b68fa67abf0690bcf5c8b527b0a2f8979bc4b8
hohung170502 <2000003074@nttu.edu.vn>
2025-05-23 09:57:47 +0700

    fix: remove broken Google login button

--- src/app/admin/profile/page.tsx
+++ src/app/admin/profile/page.tsx
@@ -1,18 +1,19 @@
-import { FormDemo } from "@/components/form-profile";
+"use client";
 
+import { FormDemo } from "@/components/form-profile";
 
 export default function ProfilePage() {
   return (
     <div className="bg-[#f5f5f5] flex  p-4">
       <div className="bg-white p-4 rounded-md shadow border w-full ">
-       <div className="border-b  ">
-         <h1 className="text-xl font-bold mb-4">Thông tin cá nhân</h1>
-       </div>
+        <div className="border-b  ">
+          <h1 className="text-xl font-bold mb-4">Thông tin cá nhân</h1>
+        </div>
         <FormDemo />
       </div>
       {/* <div className="theme-scaled">
         <FormDemo />
       </div> */}
     </div>
-  )
+  );
 }
